75ac243446a28fba404849f859ba404dee8bb3ce
Aakaive <wonseok031@gmail.com>
2024-09-19 15:29:00 +0900

    Initial commit

---
+++ src/main/java/com/sparta/eatsapp/EatsAppApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.eatsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EatsAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EatsAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/eatsapp/EatsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.eatsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EatsAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

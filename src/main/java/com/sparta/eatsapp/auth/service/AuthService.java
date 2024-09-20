f0a6392cda42511d453cf34009d44e7ebc6edcd3
Woojin1123 <boom175839@gmail.com>
2024-09-20 17:01:25 +0900

    Feat : Auth Init & 회원가입 구현

--- src/main/java/com/sparta/eatsapp/auth/service/AuthService.java
+++ src/main/java/com/sparta/eatsapp/auth/service/AuthService.java
@@ -1,8 +1,12 @@
 package com.sparta.eatsapp.auth.service;
 
+import com.sparta.eatsapp.auth.dto.request.SignupRequest;
+import com.sparta.eatsapp.auth.dto.response.SignupResponse;
+import com.sparta.eatsapp.user.entity.User;
 import com.sparta.eatsapp.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -10,4 +14,19 @@ public class AuthService {
 
   private final UserRepository userRepository;
 
+  @Transactional
+  public SignupResponse signup(SignupRequest signupRequest) {
+
+    String encryptedPassword = signupRequest.getPwd();
+    User user = new User(
+        signupRequest.getEmail(),
+        encryptedPassword,
+        signupRequest.getName(),
+        signupRequest.getAddress(),
+        signupRequest.getRole(),
+        signupRequest.getNickname()
+    );
+    User savedUser = userRepository.save(user);
+    return new SignupResponse(savedUser);
+  }
 }
--- src/main/java/com/sparta/eatsapp/user/entity/User.java
+++ src/main/java/com/sparta/eatsapp/user/entity/User.java
@@ -5,8 +5,12 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Getter
+@NoArgsConstructor
 public class User {
 
   @Id
@@ -21,8 +25,19 @@ public class User {
   private int market_count;
   private String name;
   private String nickname;
+  private String address;
 
   @Column(columnDefinition = "TINYINT(1)")
   private boolean is_deleted;
+
+  public User(String email, String encryptedPassword, String name, String address, String role,
+      String nickname) {
+    this.email = email;
+    this.password = encryptedPassword;
+    this.name = name;
+    this.address = address;
+    this.role = role;
+    this.nickname = nickname;
+  }
 
 }

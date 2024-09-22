f972c66693ffb44133435238f71b8f58ffb2734e
Bongwon <movieid@naver.com>
2024-09-22 21:26:01 +0900

    [Add] 배달 완료 후 리뷰 작성

--- src/main/java/com/sparta/eatsapp/review/service/ReviewService.java
+++ src/main/java/com/sparta/eatsapp/review/service/ReviewService.java
@@ -1,13 +1,13 @@
 package com.sparta.eatsapp.review.service;
 
 import com.sparta.eatsapp.order.entity.Order;
+import com.sparta.eatsapp.order.entity.OrderStatus;
 import com.sparta.eatsapp.order.service.OrderService;
 import com.sparta.eatsapp.review.dto.ReviewRequestDto;
 import com.sparta.eatsapp.review.dto.ReviewResponseDto;
 import com.sparta.eatsapp.review.entity.Review;
 import com.sparta.eatsapp.review.repository.ReviewRepository;
 import lombok.RequiredArgsConstructor;
-import org.aspectj.weaver.ast.Or;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -18,19 +18,24 @@ import java.util.List;
 @RequiredArgsConstructor
 public class ReviewService {
 
-    private final ReviewRepository reviewReqository;
+    private final ReviewRepository reviewRepository;
     private final OrderService orderService;
 
     public ReviewResponseDto save(ReviewRequestDto requestDto) {
         Order order = findByOrderId(requestDto.getOrderId());
+
+        if(!order.getOrderStatus().equals(OrderStatus.FINISH)) {
+            throw new IllegalArgumentException("배달이 완료된 후에 리뷰를 작성할 수 있습니다.");
+        }
+
         Review review = new Review(requestDto, order);
-        Review savedReview = reviewReqository.save(review);
+        Review savedReview = reviewRepository.save(review);
 
         return new ReviewResponseDto(savedReview, order);
     }
 
     public List<ReviewResponseDto> getReviews(Long marketId) {
-        List<Review> reviews = reviewReqository.findAllByRestaurantId(marketId);
+        List<Review> reviews = reviewRepository.findAllByRestaurantId(marketId);
 
         List<ReviewResponseDto> reviewLists = new ArrayList<>();
         for(Review reviewList : reviews){
@@ -43,7 +48,7 @@ public class ReviewService {
 
     @Transactional
     public ReviewResponseDto modifiedReview(Long reviewId, ReviewRequestDto requestDto) {
-        Review review = reviewReqository.findById(reviewId).orElseThrow(() ->
+        Review review = reviewRepository.findById(reviewId).orElseThrow(() ->
                 new IllegalArgumentException("유효한 리뷰가 아닙니다."));
 
         Order order = findByOrderId(review.getOrderId());

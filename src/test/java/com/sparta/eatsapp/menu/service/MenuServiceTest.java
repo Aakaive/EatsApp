d36b2850fac52e817c09f4eb1b3b0c2593b43783
Woojin1123 <boom175839@gmail.com>
2024-09-25 09:38:12 +0900

    [FIX] import 수정

--- src/test/java/com/sparta/eatsapp/menu/service/MenuServiceTest.java
+++ src/test/java/com/sparta/eatsapp/menu/service/MenuServiceTest.java
@@ -1,9 +1,6 @@
 package com.sparta.eatsapp.menu.service;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.any;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
